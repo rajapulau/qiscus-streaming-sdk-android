8b3d2721b92594e6488e38c30dc893027c46a969
Fitra Aditya <fitra@g.pl>
2017-09-28 16:14:27 +0700

    Remove permission check.

--- app/src/main/java/com/qiscus/streaming/sample/MainActivity.java
+++ app/src/main/java/com/qiscus/streaming/sample/MainActivity.java
@@ -14,21 +14,10 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
 
-    private final String[] PERMISSIONS = {
-        Manifest.permission.CAMERA,
-        Manifest.permission.INTERNET,
-        Manifest.permission.RECORD_AUDIO,
-        Manifest.permission.WAKE_LOCK,
-    };
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        if (!hasPermissions(this, PERMISSIONS)) {
-            ActivityCompat.requestPermissions(this, PERMISSIONS, 1);
-        }
 
         overridePendingTransition(R.transition.slide_in, R.transition.slide_out);
 
@@ -46,16 +35,5 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this, BasicStreamActivity.class));
             }
         });
-    }
-
-    public boolean hasPermissions(Context context, String... permissions) {
-        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && context != null && permissions != null) {
-            for (String permission : permissions) {
-                if (ActivityCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED) {
-                    return false;
-                }
-            }
-        }
-        return true;
     }
 }
