55c3420e5d46cb4709820558703578156d725b0f
Fitra Aditya <fitra@g.pl>
2017-09-28 14:34:13 +0700

    Add setter getter.

--- qiscus-streaming/src/main/java/com/qiscus/streaming/data/QiscusStream.java
+++ qiscus-streaming/src/main/java/com/qiscus/streaming/data/QiscusStream.java
@@ -1,13 +1,90 @@
 package com.qiscus.streaming.data;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created by fitra on 27/09/17.
  */
 
-public class QiscusStream {
+public class QiscusStream implements Parcelable {
     public String streamName;
     public String streamToken;
     public String streamUrl;
     public String playUrl;
     public String hlsUrl;
+
+    protected QiscusStream(Parcel in) {
+        streamName = in.readString();
+        streamToken = in.readString();
+        streamUrl = in.readString();
+        playUrl = in.readString();
+        hlsUrl = in.readString();
+    }
+
+    public void setStreamName(String name) {
+        streamName = name;
+    }
+
+    public void setStreamToken(String token) {
+        streamToken = token;
+    }
+
+    public void setStreamUrl(String url) {
+        streamUrl = url;
+    }
+
+    public void setPlayUrl(String url) {
+        playUrl = url;
+    }
+
+    public void setHlsUrl(String url) {
+        hlsUrl = url;
+    }
+
+    public String getStreamName() {
+        return streamName;
+    }
+
+    public String getStreamToken() {
+        return streamToken;
+    }
+
+    public String getStreamUrl() {
+        return streamUrl;
+    }
+
+    public String getPlayUrl() {
+        return playUrl;
+    }
+
+    public String getHlsUrl() {
+        return hlsUrl;
+    }
+
+    public static final Creator<QiscusStream> CREATOR = new Creator<QiscusStream>() {
+        @Override
+        public QiscusStream createFromParcel(Parcel in) {
+            return new QiscusStream(in);
+        }
+
+        @Override
+        public QiscusStream[] newArray(int size) {
+            return new QiscusStream[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(streamName);
+        dest.writeString(streamToken);
+        dest.writeString(streamUrl);
+        dest.writeString(playUrl);
+        dest.writeString(hlsUrl);
+    }
 }
--- qiscus-streaming/src/main/java/com/qiscus/streaming/data/QiscusStreamParameter.java
+++ qiscus-streaming/src/main/java/com/qiscus/streaming/data/QiscusStreamParameter.java
@@ -1,12 +1,63 @@
 package com.qiscus.streaming.data;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created by fitra on 28/09/17.
  */
 
-public class QiscusStreamParameter {
+public class QiscusStreamParameter implements Parcelable {
     public int videoWidth = 320;
     public int videoHeight = 240;
     public int videoFps = 25;
     public int videoBitrate = 350 * 1024;
+
+    protected QiscusStreamParameter(Parcel in) {
+        videoWidth = in.readInt();
+        videoHeight = in.readInt();
+        videoFps = in.readInt();
+        videoBitrate = in.readInt();
+    }
+
+    public void setVideoWidth(int width) {
+        videoWidth = width;
+    }
+
+    public void setVideoHeight(int height) {
+        videoHeight = height;
+    }
+
+    public void setVideoFps(int fps) {
+        videoFps = fps;
+    }
+
+    public void setVideoBitrate(int bitrate) {
+        videoBitrate = bitrate;
+    }
+
+    public static final Creator<QiscusStreamParameter> CREATOR = new Creator<QiscusStreamParameter>() {
+        @Override
+        public QiscusStreamParameter createFromParcel(Parcel in) {
+            return new QiscusStreamParameter(in);
+        }
+
+        @Override
+        public QiscusStreamParameter[] newArray(int size) {
+            return new QiscusStreamParameter[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(videoWidth);
+        dest.writeInt(videoHeight);
+        dest.writeInt(videoFps);
+        dest.writeInt(videoBitrate);
+    }
 }
