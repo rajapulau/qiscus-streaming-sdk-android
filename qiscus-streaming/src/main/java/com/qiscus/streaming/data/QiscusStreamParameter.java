6ae3f5108142f8a444c631a2fb1f40d8aa6971f3
Fitra Aditya <fitra@g.pl>
2017-09-28 13:39:38 +0700

    Add stream parameter object.

---
+++ qiscus-streaming/src/main/java/com/qiscus/streaming/data/QiscusStreamParameter.java
@@ -0,0 +1,12 @@
+package com.qiscus.streaming.data;
+
+/**
+ * Created by fitra on 28/09/17.
+ */
+
+public class QiscusStreamParameter {
+    public int videoWidth = 320;
+    public int videoHeight = 240;
+    public int videoFps = 25;
+    public int videoBitrate = 350 * 1024;
+}
