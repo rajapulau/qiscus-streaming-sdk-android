dfa5b1fa5dbd880e02828e1a1591c2408c564dfc
Fitra Aditya <fitra@g.pl>
2017-09-28 13:40:13 +0700

    Make properties public and remove setter getter.

--- qiscus-streaming/src/main/java/com/qiscus/streaming/data/QiscusStream.java
+++ qiscus-streaming/src/main/java/com/qiscus/streaming/data/QiscusStream.java
@@ -5,49 +5,9 @@ package com.qiscus.streaming.data;
  */
 
 public class QiscusStream {
-    private String streamName;
-    private String streamToken;
-    private String streamUrl;
-    private String playUrl;
-    private String hlsUrl;
-
-    public void setStreamName(String name) {
-        streamName = name;
-    }
-
-    public void setStreamToken(String token) {
-        streamToken = token;
-    }
-
-    public void setStreamUrl(String url) {
-        streamUrl = url;
-    }
-
-    public void setPlayUrl(String url) {
-        playUrl = url;
-    }
-
-    public void setHlsUrl(String url) {
-        hlsUrl = url;
-    }
-
-    public String getStreamName() {
-        return streamName;
-    }
-
-    public String getStreamToken() {
-        return streamToken;
-    }
-
-    public String getStreamUrl() {
-        return streamUrl;
-    }
-
-    public String getPlayUrl() {
-        return playUrl;
-    }
-
-    public String getHlsUrl() {
-        return hlsUrl;
-    }
+    public String streamName;
+    public String streamToken;
+    public String streamUrl;
+    public String playUrl;
+    public String hlsUrl;
 }
